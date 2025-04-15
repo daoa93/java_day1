0cbfc8fc99e67c9b488d7dd509618f6ee59c0d9d
daoa93 <daoa93@naver.com>
2025-04-15 10:45:26 +0900

    feat: scanner 연습

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("이름을 입력해주세요:");
+        String name = scanner.nextLine();
+
+        System.out.print("나이를 입력해주세요:");
+        int age = scanner.nextInt();
+
+        System.out.print("키를 입력해주세요:");
+        double height = scanner.nextDouble();
+
+        System.out.print(name + "\n" + age + "\n" + height);
     }
 }
+
